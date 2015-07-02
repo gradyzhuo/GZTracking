cc4cf0ab104976de18b8421a71d1bf526e217523
Grady Zhuo <gradyzhuo@gmail.com>
2015-07-02 12:12:39 +0800

    1. add example project

---
+++ Project/Example/Example/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Grady Zhuo on 7/1/15.
+//  Copyright © 2015 Grady Zhuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.layoutMargins = UIEdgeInsets(top: <#T##CGFloat#>, left: <#T##CGFloat#>, bottom: <#T##CGFloat#>, right: <#T##CGFloat#>)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
